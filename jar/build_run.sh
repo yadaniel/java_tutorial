098fbfad5936362aa1f5a6fbc9ace40e1e12dca6
yadaniel <ya.daniel@inbox.ru>
2022-02-17 12:46:23 +0100

    update2

--- jar/build_run.sh
+++ jar/build_run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 rm -f *.{class,jar}
- javac *.java
- mkdir META-INF && echo "Main-Class: Main" > META-INF/MANIFEST.MF
- jar cfm MainManifest.jar META-INF/MANIFEST.MF *.class
- java -jar MainManifest.jar
+javac *.java
+mkdir META-INF && echo "Main-Class: Main" > META-INF/MANIFEST.MF
+jar cfm MainManifest.jar META-INF/MANIFEST.MF *.class
+java -jar MainManifest.jar
 
